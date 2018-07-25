937e09b3e2395e68dad97ec4ce03e3436735a7ac
yhishi <hishikawakeiki@hishi-Mac.local>
2018-07-25 22:05:06 +0900

    implements bookmark page

--- src/pages/bookmark/bookmark.ts
+++ src/pages/bookmark/bookmark.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { IonicPage, NavController, NavParams, ItemSliding, ToastController } from 'ionic-angular';
+import { BookmarkProvider } from '../../providers/bookmark/bookmark';
 /**
  * Generated class for the BookmarkPage page.
  *
@@ -15,11 +15,51 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class BookmarkPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  events: any[] = [];
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public toastCtrl: ToastController,
+    public bookmarkProvider: BookmarkProvider
+  ){}
+
+  ionViewWillEnter() {
+    console.log('ionViewWillEnter BookmarkPage');
+    this.bookmarkProvider.get().then(events => {
+      this.events = this.toEventArray(events);
+    });
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad BookmarkPage');
+  private toEventArray(events): Array<any> {
+    const eventArray = [];
+    Object.keys(events).forEach(key => {
+      eventArray.push(events[key]);
+    })
+    eventArray.sort((ev1, ev2) => {
+      let ret = ev2.started_at.localeCompare(ev1.started_at);
+      if (ret !== 0) return ret;
+      return ev2.event_id - ev1.event_id;
+    })
+    return eventArray;
   }
 
+  openEvent(event) {
+    this.navCtrl.push('EventDetailPage', {
+      eventId: event.event_id,
+      event: event
+    });
+  }
+
+  doDelete(event:any, itemSliding: ItemSliding) {
+    this.bookmarkProvider.delete(event).then(events => {
+      this.events = this.toEventArray(events);
+      const toast = this.toastCtrl.create({
+        message: 'ブックマークを削除しました。',
+        duration: 1500
+      });
+      toast.present();
+    })
+    itemSliding.close();
+  }
 }
